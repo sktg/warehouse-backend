d08c50f47ddc3dae66fc58f75b79a979a8dab1fe
satish kumar <sktg.1988@gmail.com>
2026-02-03 19:22:14 +0000

    render using temp

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./warehouse.db"
+#using temporary file-based SQLite database for persistence of render
+DATABASE_URL = "sqlite:////tmp/warehouse.db"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
